b6c158fc2741a4d3d42fb12e1462f9ad106ad65e
Gaurav Kumar Singh <55310399+singhgaurav24@users.noreply.github.com>
2022-03-06 20:38:41 +0530

    Update test_GoogleUrl.py

--- testCase/test_GoogleUrl.py
+++ testCase/test_GoogleUrl.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Optionsoptions = Options()
+from selenium.webdriver.chrome.options import Options
+options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
