caf67257ec70fd39d62532c71d5ea818b998744e
Gaurav Kumar Singh <55310399+singhgaurav24@users.noreply.github.com>
2022-03-06 20:24:36 +0530

    Update test_GoogleUrl.py

--- testCase/test_GoogleUrl.py
+++ testCase/test_GoogleUrl.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
-import pytest
-
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-baseUrl = "https://www.flipkart.com/"
-s = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)\
-
-driver.get(baseUrl)
-driver.quit()
+from selenium.webdriver.chrome.options import Optionsoptions = Options()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.google.com/")
+element_text = driver.page_source
+print(element_text)
